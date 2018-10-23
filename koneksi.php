4459a981ad09b2695922dfd07ce3aef8fdf937d7
restufiqihakh <restufiqih111@gmail.com>
2018-10-23 19:48:58 +0700

    php_input

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,14 @@
 <?php  
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+$servername = 'localhost';//host database
+$username = 'root';//username database
+$password = '';//password database
 $database = 'akhdiyat';
 
 $conn = new mysqli($servername,$username,$password,$database);
 
 if($conn->connect_error){
 	die("Koneksi Error");
+} else{
+	echo "Koneksi Berhasil";
 }
-// echo "Koneksi Berhasil"
 ?>
